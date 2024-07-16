937d56b0391b718800af3e1c18d71a717207403d
x_air <unchainedsoul@nate.com>
2024-07-16 23:36:23 +0900

    Initial Commit

---
+++ x_ball/x_ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_ballApp.swift
+//  x_ball
+//
+//  Created by MIN GI KIM on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct x_ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
