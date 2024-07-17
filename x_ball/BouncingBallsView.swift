5f6cddd1a6c64d84dc54ea373cdc01f757400c91
x_air <unchainedsoul@nate.com>
2024-07-17 21:08:05 +0900

    두 화면으로 분할

--- x_ball/BouncingBallsView.swift
+++ x_ball/BouncingBallsView.swift
@@ -1,8 +1,50 @@
-//
-//  BouncingBallsView.swift
-//  x_ball
-//
-//  Created by MIN GI KIM on 7/17/24.
-//
+import SwiftUI
+import UIKit
 
-import Foundation
+struct BouncingBallsView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: UIScreen.main.bounds)
+        let animator = UIDynamicAnimator(referenceView: view)
+        
+        let blueBall = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
+        blueBall.backgroundColor = .blue
+        blueBall.layer.cornerRadius = 25
+        view.addSubview(blueBall)
+        
+        let redBall = UIView(frame: CGRect(x: 200, y: 100, width: 50, height: 50))
+        redBall.backgroundColor = .red
+        redBall.layer.cornerRadius = 25
+        view.addSubview(redBall)
+        
+        let gravity = UIGravityBehavior(items: [blueBall, redBall])
+        animator.addBehavior(gravity)
+        
+        let collision = UICollisionBehavior(items: [blueBall, redBall])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
+        let dynamicBehavior = UIDynamicItemBehavior(items: [blueBall, redBall])
+        dynamicBehavior.elasticity = 0.8
+        animator.addBehavior(dynamicBehavior)
+        
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+            let randomXBlue = CGFloat.random(in: -100...100)
+            let randomYBlue = CGFloat.random(in: -100...100)
+            let randomXRed = CGFloat.random(in: -100...100)
+            let randomYRed = CGFloat.random(in: -100...100)
+            
+            dynamicBehavior.addLinearVelocity(CGPoint(x: randomXBlue, y: randomYBlue), for: blueBall)
+            dynamicBehavior.addLinearVelocity(CGPoint(x: randomXRed, y: randomYRed), for: redBall)
+        }
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+struct BouncingBallsView_Previews: PreviewProvider {
+    static var previews: some View {
+        BouncingBallsView()
+    }
+}
--- x_ball/ContentView.swift
+++ x_ball/ContentView.swift
@@ -1,53 +1,33 @@
 import SwiftUI
-import UIKit
 
 struct ContentView: View {
     var body: some View {
-        BouncingBallsView()
-            .edgesIgnoringSafeArea(.all)
+        NavigationView {
+            VStack {
+                Text("Main Screen")
+                    .font(.largeTitle)
+                    .padding()
+
+                NavigationLink(destination: BouncingBallsViewContainer()) {
+                    Text("Go to Bouncing Balls")
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .navigationTitle("Home")
+        }
     }
 }
 
-struct BouncingBallsView: UIViewRepresentable {
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
-        let animator = UIDynamicAnimator(referenceView: view)
-        
-        let blueBall = UIView(frame: CGRect(x: 100, y: 100, width: 50, height: 50))
-        blueBall.backgroundColor = .blue
-        blueBall.layer.cornerRadius = 25
-        view.addSubview(blueBall)
-        
-        let redBall = UIView(frame: CGRect(x: 200, y: 100, width: 50, height: 50))
-        redBall.backgroundColor = .red
-        redBall.layer.cornerRadius = 25
-        view.addSubview(redBall)
-        
-        let gravity = UIGravityBehavior(items: [blueBall, redBall])
-        animator.addBehavior(gravity)
-        
-        let collision = UICollisionBehavior(items: [blueBall, redBall])
-        collision.translatesReferenceBoundsIntoBoundary = true
-        animator.addBehavior(collision)
-        
-        let dynamicBehavior = UIDynamicItemBehavior(items: [blueBall, redBall])
-        dynamicBehavior.elasticity = 0.8
-        animator.addBehavior(dynamicBehavior)
-        
-        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
-            let randomXBlue = CGFloat.random(in: -100...100)
-            let randomYBlue = CGFloat.random(in: -100...100)
-            let randomXRed = CGFloat.random(in: -100...100)
-            let randomYRed = CGFloat.random(in: -100...100)
-            
-            dynamicBehavior.addLinearVelocity(CGPoint(x: randomXBlue, y: randomYBlue), for: blueBall)
-            dynamicBehavior.addLinearVelocity(CGPoint(x: randomXRed, y: randomYRed), for: redBall)
-        }
-        
-        return view
+struct BouncingBallsViewContainer: View {
+    var body: some View {
+        BouncingBallsView()
+            .edgesIgnoringSafeArea(.all)
+            .navigationTitle("Bouncing Balls")
     }
-    
-    func updateUIView(_ uiView: UIView, context: Context) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
